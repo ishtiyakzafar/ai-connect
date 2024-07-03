e1543136d1547df87481785cfbbffcecc0712e6f
ishtiyaque.jaffer@iifl.com <ishtiyaque.jaffer>
2024-07-03 12:32:30 +0530

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'myapp',
+  appId: 'com.aiconnect.app',
+  appName: 'aiconnect',
   webDir: 'www'
 };
 
